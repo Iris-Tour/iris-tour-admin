1d398564c104500330e56307e789b66e3ffa69c0
giovanni <giovidoh@gmail.com>
2025-04-03 22:03:14 +0000

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+    return (
+        <div className="flex w-full h-full">
+            <div></div>
+            <div className="bg-login-image w-1/2 h-full"></div>
+        </div>
+    );
+};
+
+export default LoginPage;
